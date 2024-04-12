850236093775f8fa15ce048ce232a19d535f4735
Bektemir2001 <bektemirkumarbayuulu@gmail.com>
2024-04-12 20:48:22 +0600

    fix audio

--- speech2text.py
+++ speech2text.py
@@ -8,9 +8,12 @@ class WhisperModel:
         self.device = "cuda:" + str(device)
         self.model = WhisperForConditionalGeneration.from_pretrained(config.get("model_path")).to(device)
         self.processor = WhisperProcessor.from_pretrained(config.get("processor_path"))
+        self.config = config
 
     def generate_text_from_audio(self, audio_file_path):
-        audio_input, _ = torchaudio.load(audio_file_path)  # Загрузка файла
+        wav_file_path = config('UPLOAD_FOLDER') + "/" + str(randint(1000000, 9999999)) + "#" + str(randint(1000000, 999999999)) + ".wav"
+        self.convert_to_wav(audio_file_path, wav_file_path)
+        audio_input, _ = torchaudio.load(wav_file_name)
         if audio_input.shape[0] == 2:
             audio_input = audio_input.mean(dim=0, keepdim=True)
 
@@ -21,7 +24,13 @@ class WhisperModel:
             outputs = self.model.generate(**inputs)
 
         generated_text = self.processor.decode(outputs[0].to(self.device), skip_special_tokens=True)
+        os.remove(wav_file_path)
+        os.remove(audio_file_path)
         return generated_text
 
- 
+    def convert_to_wav(self, input_file, output_file):
+        audio = AudioSegment.from_file(input_file)
+        audio = audio.set_channels(1).set_frame_rate(16000)
+        audio = audio.set_sample_width(2)
+        audio.export(output_file, format="wav")
 
