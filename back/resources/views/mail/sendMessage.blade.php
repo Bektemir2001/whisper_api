9014f8e89882ea7c18e6d1cf87091f7c23a9d901
Bektemir2001 <bektemirkumarbayuulu@gmail.com>
2024-04-15 18:50:58 +0600

    save changes

--- back/resources/views/mail/sendMessage.blade.php
+++ back/resources/views/mail/sendMessage.blade.php
@@ -75,7 +75,7 @@
                                 видео инструкция
                             </th>
                             <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 ">
-                                POST
+                                <a href="https://www.youtube.com/watch?v=bfZWTS7bcus&t=45s">https://www.youtube.com/watch?v=bfZWTS7bcus&t=45s</a>
                             </td>
                         </tr>
 
